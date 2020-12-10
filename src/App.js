aa26cd69a5cb5689c4da32a8cf5bb0dd80beef41
Sergey Biryukov <2700036@mail.ru>
2020-12-10 20:20:03 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import Tabs from './components/Tabs';
 import AddressBar from './components/AddressBar';
 import './App.css';
 
+
+function reducer (state, {type, payload}) {
+  const {browsers, activeBrowser} = state;
+  switch (type) {
+    case 'CHOOSE':
+      return { 
+        ...state, activeBrowser: payload 
+      }      
+    case 'ADD':
+      return { 
+        ...state, 
+        browsers: [...browsers, ''],
+        activeBrowser: browsers.length
+      }
+    case 'UPDATE':
+      const newBrowsers = [...browsers];
+      newBrowsers[activeBrowser] = payload;
+      return { 
+        ...state, browsers: newBrowsers
+      } 
+    case 'CLOSE':
+      return { 
+        ...state, ...payload 
+      }  
+    default:
+      return state
+    }
+}
+
 export default function App() {
-  const [browsers, setBrowsers] = useState([
-    'https://chrome.google.com',
-    'https://biryukov.netlify.app',
-  ]);
-  const [activeBrowser, setActiveBrowser] = useState(0)
-
-  const chooseBrowser = (i) => {
-    setActiveBrowser(i)
-  }
-
-  const addBrowser = () => {
-    setBrowsers(browsers=>[...browsers, ''])
-    setActiveBrowser(browsers.length)
-  }
-  const updateBrowser = (value) => {
-    const newBrowsers = [...browsers];
-    newBrowsers[activeBrowser] = value
-    setBrowsers(newBrowsers)
-  }
-
-  const url = browsers[activeBrowser];
+  const [{browsers, activeBrowser}, dispatch] = useReducer(reducer, {
+    browsers: [
+      'https://chrome.google.com',
+      'https://biryukov.netlify.app',
+    ],
+    activeBrowser: 0
+  });
   
+  const chooseBrowser = (payload) => dispatch({type: 'CHOOSE', payload});
+  const addBrowser = () => dispatch({type: 'ADD'});
+  const updateBrowser = (payload) => dispatch({type: 'UPDATE', payload});
+
+  const url = browsers[activeBrowser];  
   return (
     <div className="app">
       <div className="browser">
